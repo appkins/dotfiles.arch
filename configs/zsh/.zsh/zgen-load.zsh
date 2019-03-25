e28c53a5ea251b90c968d57bb5f14cae07dd1bd2
appkins <nbatkins@gmail.com>
2019-03-24 21:04:03 -0500

    fixed zsh for aur

--- configs/zsh/.zsh/zgen-load.zsh
+++ configs/zsh/.zsh/zgen-load.zsh
@@ -12,7 +12,7 @@
 #=======================================                                                                      
 
 # load zgen
-source "/usr/share/zsh/.zgen/zgen.zsh"
+source "/usr/share/zsh/share/zgen.zsh"
 
 # if the init scipt doesn't exist
 if ! zgen saved; then
